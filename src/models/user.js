c494d6068d4015ef705ca3ca166397fdab0ed332
Petri Sandström <hello@petrisandstrom.com>
2019-12-30 08:11:52 +0200

    Add user stub model

--- src/models/user.js
+++ src/models/user.js
@@ -1,1 +1,13 @@
 const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number
+  }
+});
+
+module.exports = User;
