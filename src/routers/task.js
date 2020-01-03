dfad7fbde51c0f8fed89a42b75c9d7b5900d7e20
Petri Sandström <hello@petrisandstrom.com>
2020-01-03 14:34:49 +0200

    Add GET task and tasks, add POST user

--- src/routers/task.js
+++ src/routers/task.js
@@ -3,4 +3,29 @@ const Task = require('../models/task');
 const auth = require('../middleware/auth');
 const router = new express.Router();
 
-module.exports = router
+router.get('/tasks', auth, async (req, res) => {
+  try {
+    await req.user.populate('tasks').execPopulate();
+    res.send(req.user.tasks);
+  } catch (error) {
+    res.status(500).send();
+  }
+})
+
+router.get('/tasks/:id', auth, async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findOne({ _id, owner: req.user._id });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,4 +3,16 @@ const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
 
-module.exports = router
+router.post('/users', async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+module.exports = router;
