42fdadceecae4257208ea8a4eb520bba7cab1b07
Petri Sandström <hello@petrisandstrom.com>
2020-01-04 21:23:58 +0200

    Add PATCH user

--- src/routers/user.js
+++ src/routers/user.js
@@ -26,6 +26,24 @@ router.delete('/users/me', auth, async (req, res) => {
   } catch (error) {
     res.status(500).send();
   }
-})
+});
+
+router.patch('/users/me', auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    updates.forEach((update) => req.user[update] = req.body[update]);
+    await req.user.save();
+    res.send(req.user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 module.exports = router;
