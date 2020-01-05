7b895b5feb028b3c929c246b0ba5a2c099f8f510
Petri Sandström <hello@petrisandstrom.com>
2020-01-05 21:58:59 +0200

    Add POST, PATCH and DELETE task endpoints

--- src/routers/task.js
+++ src/routers/task.js
@@ -10,7 +10,7 @@ router.get('/tasks', auth, async (req, res) => {
   } catch (error) {
     res.status(500).send();
   }
-})
+});
 
 router.get('/tasks/:id', auth, async (req, res) => {
   const _id = req.params.id;
@@ -24,6 +24,58 @@ router.get('/tasks/:id', auth, async (req, res) => {
 
     res.send(task);
   } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.post('/tasks', auth, async (req, res) => {
+  const task = new Task({
+      ...req.body,
+      owner: req.user._id
+  });
+
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.patch('/tasks/:id', auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id});
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    updates.forEach((update) => task[update] = req.body[update]);
+    await task.save();
+    res.send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete('/tasks/:id', auth, async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
+
+    if (!task) {
+      res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
     res.status(500).send();
   }
 });
